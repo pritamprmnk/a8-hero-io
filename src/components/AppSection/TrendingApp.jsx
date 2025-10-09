aea27d691ceab649bfcacdc217eaebefe0e7ab24
Pritam Pramanik <pritampramanik115@gmail.com>
2025-10-10 02:53:56 +0600

    addded banner and trustedcard

---
+++ src/components/AppSection/TrendingApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrendingApp = () => {
+  return (
+    <div>
+      <h1>Trending App Section</h1>
+    </div>
+  )
+}
+
+export default TrendingApp
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
 import TrustedCard from '../../components/TrustedCard/TrustedCard'
+import TrendingApp from '../../components/AppSection/TrendingApp'
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <TrustedCard></TrustedCard>
+      <TrendingApp></TrendingApp>
     </div>
   )
 }
